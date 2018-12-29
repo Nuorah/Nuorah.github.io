d11c671823add84778588cb040c5a68e84e92450
Haroun Moatassime <haroun.moatassime@gmail.com>
2018-12-29 16:59:51 +0100

    Pas raciste une seule phrase sans ponctuation

--- pasRaciste.js
+++ pasRaciste.js
@@ -1,6 +1,19 @@
-const text = document.queryselector('.text')
 
-const textSubmit = document.querySelector('.guessSubmit');
-const textField = document.querySelector('.guessField');
+
+
+
+const textResult = document.querySelector('.textResult');
+
+const textSubmit = document.querySelector('.textSubmit');
+const textField = document.querySelector('.textField');
 
 let resetButton;
+
+function transformText() {
+  let text = textField.value
+  let textTransformed = "Je ne suis pas raciste, mais " + text + " si tu vois"
+  + " ce que je veux dire."
+  textResult.textContent = textTransformed
+}
+
+textSubmit.addEventListener('click', transformText);
