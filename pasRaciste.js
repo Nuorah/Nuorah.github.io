9467e7edb30fc134254654472963e9637d3a6ec3
Haroun Moatassime <haroun.moatassime@gmail.com>
2018-12-29 17:43:33 +0100

    Déracisteur final

--- pasRaciste.js
+++ pasRaciste.js
@@ -11,9 +11,12 @@ let resetButton;
 
 function transformText() {
   let text = textField.value
-  let textTransformed = "Je ne suis pas raciste, mais " + text + " si tu vois"
-  + " ce que je veux dire."
-  textResult.textContent = textTransformed
+  let textLowerCase = text.toLowerCase()
+  let textPunct = textLowerCase.replace(/\.|\!/g," si tu vois ce que je veux dire. Je ne suis pas raciste, mais")
+  let textCut= textPunct.slice(0, -29)
+  let textFinal = "Je ne suis pas raciste, mais " + textCut
+
+  textResult.textContent = textFinal
 }
 
 textSubmit.addEventListener('click', transformText);
