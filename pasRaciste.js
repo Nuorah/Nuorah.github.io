20bf28ca6ffb5f3921104cf35f10a9e828746fea
Haroun Moatassime <haroun.moatassime@gmail.com>
2018-12-29 16:41:06 +0100

    Initial commit pasRaciste

---
+++ pasRaciste.js
@@ -0,0 +1,6 @@
+const text = document.queryselector('.text')
+
+const textSubmit = document.querySelector('.guessSubmit');
+const textField = document.querySelector('.guessField');
+
+let resetButton;
